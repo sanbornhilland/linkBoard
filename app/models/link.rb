dd87859397bd7e76fbf26a26a9446067b601d7ac
sanbornhnewyyz <amadeus.hilland@gmail.com>
2017-03-02 21:16:48 -0500

    Add SSE events when a link is created.
    This is a very large flaw at the moment where the server
    does not seem to be able to shut down properly and you
    have to go in and manually kill the processes it spawned.

--- app/models/link.rb
+++ app/models/link.rb
@@ -1,3 +1,15 @@
 class Link < ApplicationRecord
 	belongs_to :board
+
+  after_create :notify_created
+
+  def notify_created
+    connection = ActiveRecord::Base.connection
+    connection.execute "NOTIFY #{channel}, #{connection.quote self.id.to_s}"
+  end
+
+  private
+  def channel
+    "board_#{self.board.id}"
+  end
 end
